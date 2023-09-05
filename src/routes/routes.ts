3d47a04d2c667d827b968e197a9ff27b8abf5a60
Pawan Kumar <iampawanmkr@gmail.com>
2023-09-05 14:27:28 +0530

    refresh token is now in action

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,15 @@
 import { Router, Request, Response } from "express";
 import { UserController } from "../controllers/index.js";
-import { 
+import {
   validateRoute, 
   userRegistrationValidationChain, 
   userLoginValidationChain 
-} from "../utils/validation/user.js";
+} from "../utils/index.js";
+import {Token} from "../controllers/index.js";
 
 export const router: Router = Router();
 
-router.post("/register", userRegistrationValidationChain, validateRoute, UserController.registerNewUser);  
+router.post("/register", userRegistrationValidationChain, validateRoute, UserController.registerNewUser);
 router.post("/login", userLoginValidationChain, validateRoute, UserController.login);
+
+router.post("/token/refresh", Token.verifyAndRefreshToken);
